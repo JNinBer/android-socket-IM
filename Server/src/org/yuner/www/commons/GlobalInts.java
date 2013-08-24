961137a05f3f2b906e12c1dd2b8caa9bce524b3c
tianlunte <wangqingyun123@yahoo.com>
2013-08-24 11:24:00 +0800

    -a

--- Server/src/org/yuner/www/commons/GlobalInts.java
+++
@@ -1,9 +0,0 @@
-package org.yuner.www.commons;
-
-public class GlobalInts {
-
-	public static final int idNotOnline = 21;
-	public static final int idRefuseFriendship = 22;
-	public static final int idAcceptFriendship = 23;
-
-}
